83edb9dd24ce77d0d607a30f482b4e7905e58357
LeoPKM <thinhphatmai2001@gmail.com>
2023-08-02 09:11:04 +0700

    fix authen

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,12 @@
 const { vertifyJWT } = require("./../utils/loginHelper");
 const userModel = require("./../models/userModel");
-
 const { Response } = require("./../utils");
+
+const NOT_HAVING_AUTH_INFOR = "not having authentication infor";
+const NOT_VERTIFIED = "NOT VERTIFIED";
+const TOKEN_NOT_MATCH = "TOKEN NOT MATCH";
+const REDIRECT_TO_LOGIN_MESS = "chuyển hướng tới trang đăng nhập";
+const MUST_LOGOUT = "cần đăng xuất ra trước";
 
 const getToken = (req) => {
 	const authHeader = req.headers["authorization"];
@@ -10,47 +15,61 @@ const getToken = (req) => {
 };
 
 const requireLogined = async (req, res, next) => {
-	const jwtToken = getToken(req);
-	let [decodeStatus, decoded] = [true, []];
-	if (jwtToken) {
-		[decodeStatus, decoded] = vertifyJWT(jwtToken);
-		const user = await userModel.getUserById(decoded.ma_nguoi_dung);
-		if (user.payload[0].token !== jwtToken) {
-			[decodeStatus, decoded] = [false, []];
+	try {
+		const jwtToken = getToken(req);
+		let [decodeStatus, decoded] = [true, []];
+		if (!jwtToken) {
+			decodeStatus = false;
+			throw new Error(NOT_HAVING_AUTH_INFOR);
 		}
-	} else {
-		decodeStatus = false;
-	}
-	if (decodeStatus) {
-		// store userlogin infor for future
-		req.auth_decoded = {
-			...decoded,
-		};
-		next();
-		return;
-	} else {
-		// có dấu hiệu của việc sử dung jwt sai
-		res.status(301).json(
-			new Response(301, [], "chuyển hướng tới trang đăng nhập")
-		);
+		[decodeStatus, decoded] = vertifyJWT(jwtToken);
+		// console.log("hehehe:", decodeStatus);
+		if (decodeStatus === false) {
+			throw new Error(NOT_VERTIFIED);
+		} else {
+			const user = await userModel.getUserById(decoded.ma_nguoi_dung);
+			if (user.payload[0].token !== jwtToken) {
+				[decodeStatus, decoded] = [false, []];
+				throw new Error(TOKEN_NOT_MATCH);
+			}
+			req.auth_decoded = {
+				...decoded,
+			};
+			next();
+			return;
+		}
+	} catch (error) {
+		if (
+			error.message === NOT_HAVING_AUTH_INFOR ||
+			error.message === NOT_VERTIFIED ||
+			error.message === TOKEN_NOT_MATCH
+		) {
+			res.status(301).json(new Response(301, [], REDIRECT_TO_LOGIN_MESS));
+		} else {
+			throw error;
+		}
 	}
 };
 
-const nonRequireLogined = (req, res, next) => {
-	const jwtToken = getToken(req);
-	let [decodeStatus, decoded] = [true, []];
-	if (jwtToken === undefined) {
+const nonRequireLogined = async (req, res, next) => {
+	try {
+		const jwtToken = getToken(req);
+		let [decodeStatus, decoded] = [false, []];
+		if (jwtToken) {
+			[decodeStatus, decoded] = vertifyJWT(jwtToken);
+			if (decodeStatus) {
+				const user = await userModel.getUserById(decoded.ma_nguoi_dung);
+				if (user.payload[0].token === jwtToken) {
+					throw new Error(MUST_LOGOUT);
+				}
+			}
+		}
 		next();
 		return;
-	} else {
-		[decodeStatus, decoded] = vertifyJWT(jwtToken);
-	}
-
-	if (!decodeStatus) {
-		next();
-		return;
-	} else {
-		res.status(400).json(new Response(400, [], "cần đăng xuất ra trước"));
+	} catch (error) {
+		if (error.message === MUST_LOGOUT) {
+			res.status(400).json(new Response(400, [], MUST_LOGOUT));
+		}
 	}
 };
 
